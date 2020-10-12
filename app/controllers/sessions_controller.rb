abbde646752441f2ed54529ed2304902055517a3
ViolaCello <ViolaCelloNYC@gmail.com>
2020-10-12 16:39:34 -0400

    login fields

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,4 +4,14 @@ def index
 end
 
 
+def create
+    @user = User.find_by(email: params[:email])
+    if @user && @user.authenticate(params[:password])
+       sessions[:user_id] = @user.id
+       redirect_to user_path
+    else
+       redirect_to '/login'
+    end
+ end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,10 +9,19 @@ def new
 end
 
 def create
-   
+    @user = User.create(user_params)
+        session[:user_id] = @user.id
+        redirect_to use_path
 end
     
 def show
+end
+
+
+private
+
+def user_params
+    params.require(:user).permit(:name, :password, :company, :email)
 end
 
 
