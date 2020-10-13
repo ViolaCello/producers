42fd2af277437057e13e721239da4ffdb9b44258
ViolaCello <ViolaCelloNYC@gmail.com>
2020-10-13 10:03:02 -0400

    debug login and sign up flow

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
+  resources :users do 
+  resources :events
+  end
   resources :events
   resources :artists
-  resources :users
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get '/', to: 'sessions#index'
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
+
+
 
 end
