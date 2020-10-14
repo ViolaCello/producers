1e80b558e58093125e51ad6fc0178f674f922143
ViolaCello <ViolaCelloNYC@gmail.com>
2020-10-14 11:41:37 -0400

    created location class method in Venue model

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,4 +1,14 @@
 class Venue < ApplicationRecord
     has_many :events
     has_many :users, through: :events
+
+
+def location
+    city = self.city
+    state = self.state
+    location = city + ", " + state
+end
+
+
+
 end
