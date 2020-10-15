1aa69ca5d88636d6afd859b5dce1882988383711
ViolaCello <ViolaCelloNYC@gmail.com>
2020-10-15 15:47:30 -0400

    added chainable scope method #by_state

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -6,13 +6,17 @@ class Venue < ApplicationRecord
     validates :state, presence: true
     validates :seats, presence: true
     scope :large, -> { where 'seats > ?', 1000 }
-
+    scope :by_state, ->(input) { where("state = ?", input) }
 
 def location
     city = self.city
     state = self.state
     "#{city}, #{state}"
 end
+
+# def self.by_state(town)
+#     self.where("state = ?", town)
+# end
 
 
 
