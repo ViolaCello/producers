9b88e79d021d4d108b5ead8b68e4d5878208959f
ViolaCello <ViolaCelloNYC@gmail.com>
2020-10-16 09:58:02 -0400

    strftime methods in Events Model

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,12 @@ class Event < ApplicationRecord
   belongs_to :user
   belongs_to :venue
   validates :name, presence: true
+end
 
-  
+def nicedate
+  self.strftime("%b %-d, %Y")
+end
+
+def nicetime
+  self.strftime("%-I:%M %P")
 end
